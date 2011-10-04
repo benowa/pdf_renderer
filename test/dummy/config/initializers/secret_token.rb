107e678a6734334e10127b12fd02aa2d0ca67658
benoit richard <benowa.desu@gmail.com>
2011-10-04 14:29:36 -0400

    pdfgem

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3ebd96864c549b202e46b1d24d9f4a7abd140578e8b36470df0f5f7341214c8e2447e16f6aaffcb5d42f5d8e7da904b33e7887c641d07fb20ed26b216b1fee96'
